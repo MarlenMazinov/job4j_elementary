5e2dca967c7ede586ce2220d9ba15580d1808978
Marlen Mazinov <maraton.bashirov@yandex.ru>
2021-10-12 19:11:58 +0300

    5.7. Ипотека [#156315 #214976] Исправление

--- src/test/java/ru/job4j/loop/Mortgage.java
+++ src/test/java/ru/job4j/loop/Mortgage.java
@@ -2,10 +2,10 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
-        int year = 1;
-        percent = percent / 100 + 1;
-        while ((amount * percent) >= salary) {
-            amount = amount * percent - salary;
+        int year = 0;
+        percent = percent / 100;
+        while (amount > 0) {
+            amount = amount * (percent + 1) - salary;
             year++;
         }
         return year;
