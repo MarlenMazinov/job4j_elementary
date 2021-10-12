b055713c4cc928798bab441287c553b58c1d4096
Marlen Mazinov <maraton.bashirov@yandex.ru>
2021-10-12 18:57:25 +0300

    5.5. Простое число [#171694 #214948] Исправление

--- src/main/java/ru/job4j/live/Loop.java
+++ src/main/java/ru/job4j/live/Loop.java
@@ -7,3 +7,12 @@ public class Loop {
         }
     }
 }
+
+class Game {
+    public static int checkGame(double percent, int prize, int pay) {
+        if (percent * prize > pay) {
+            return (int) (percent * prize - pay);
+        }
+        return 0;
+    }
+}
