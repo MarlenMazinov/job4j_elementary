8a0ab72e20a23a8a4caf6aeff9f972a00c5aae22
Marlen Mazinov <maraton.bashirov@yandex.ru>
2021-10-13 07:19:36 +0300

    5.7. Ипотека [#156315 #214976] Исправление

--- src/test/java/ru/job4j/loop/Mortgage.java
+++ src/test/java/ru/job4j/loop/Mortgage.java
@@ -5,7 +5,7 @@ public class Mortgage {
         int year = 0;
         percent = percent / 100;
         while (amount > 0) {
-            amount = amount * (percent + 1) - salary;
+            amount += amount * percent - salary;
             year++;
         }
         return year;
